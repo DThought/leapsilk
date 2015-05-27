de1597331fed0df3f304fdddebaf8b1af97a9508
Deep Thought <ti.42.plus@gmail.com>
2015-05-26 22:45:12 -0700

    testing autocrlf

--- Assets/Scripts/PaintFinger.cs
+++ Assets/Scripts/PaintFinger.cs
@@ -3,25 +3,25 @@ using System.Collections;
 using Leap;
 
 public class PaintFinger : SkeletalFinger {
-	public Color color;
-	public VoxelGrid canvas;
+  public Color color;
+  public VoxelGrid canvas;
 
-	Transform lastVoxel;
+  Transform lastVoxel;
 
-	public override void UpdateFinger() {
-		base.UpdateFinger();
+  public override void UpdateFinger() {
+    base.UpdateFinger();
 
-		// TODO: check TipVelocity to ensure movement
-		if (GetLeapFinger().IsExtended) {
-			Vector3 tip = GetTipPosition();
-			Transform voxel = canvas.GetVoxel(tip);
+    // TODO: check TipVelocity to ensure movement
+    if (GetLeapFinger().IsExtended) {
+      Vector3 tip = GetTipPosition();
+      Transform voxel = canvas.GetVoxel(tip);
 
-			if (voxel != lastVoxel) {
-				lastVoxel = voxel;
-			}
-		}
+      if (voxel != lastVoxel) {
+        lastVoxel = voxel;
+      }
+    }
 
-		GUI.Label(new Rect(10, 10, 100, 20), GetTipPosition().ToString());
-		GUI.Label(new Rect(10, 30, 100, 20), GetLeapFinger().TipVelocity.ToString());
-	}
+    GUI.Label(new Rect(10, 10, 100, 20), GetTipPosition().ToString());
+    GUI.Label(new Rect(10, 30, 100, 20), GetLeapFinger().TipVelocity.ToString());
+  }
 }
