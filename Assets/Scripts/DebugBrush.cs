e31467a994955dc6977107d3d3e459c89f9e2cb8
Deep Thought <ti.42.plus@gmail.com>
2015-05-30 09:58:23 -0700

    fixed ArgumentOutOfRange exception in brushes

--- Assets/Scripts/DebugBrush.cs
+++ Assets/Scripts/DebugBrush.cs
@@ -4,6 +4,10 @@ using Leap;
 
 public class DebugBrush : Brush {
   public override void Paint(VoxelGrid canvas, List<Vector3> points) {
+    if (points.Count == 0) {
+      return;
+    }
+
     canvas.DrawVoxel(points[points.Count - 1], pressure);
   }
 }
--- Assets/Scripts/StraightBrush.cs
+++ Assets/Scripts/StraightBrush.cs
@@ -4,6 +4,10 @@ using Leap;
 
 public class StraightBrush : Brush {
   public override void Paint(VoxelGrid canvas, List<Vector3> points) {
+    if (points.Count < 2) {
+      return;
+    }
+
     canvas.DrawLine(points[points.Count - 1], points[points.Count - 2],
         pressure);
   }
